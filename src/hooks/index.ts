c62434ab193926648f4b2676a6fb7d7bdf35b7f1
lespineira94 <lespineira94@users.noreply.github.com>
2025-12-16 02:06:56 +0000

    deploy: b394e678ec59a282428bc63a8b7de812540fabd3

--- src/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export { useGameConnection } from './useGameConnection';
--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-exportar todos los tipos
-export * from './card';
-export * from './player';
-export * from './game';
-export * from './room';
